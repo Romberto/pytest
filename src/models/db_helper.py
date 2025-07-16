011134f00637ef26262029b1499c963edb95e3b0
Romberto <korhoff@mail.ru>
2025-07-16 14:02:52 +0400

    коннект с тестовой базой данных

--- src/models/db_helper.py
+++ src/models/db_helper.py
@@ -10,7 +10,7 @@ class DataBaseHelper:
             echo=echo,
             echo_pool=echo_pool,
             max_overflow =max_overflow,
-            pool_size=pool_size
+            pool_size=pool_size,
             )
 
         self.session_factory:async_sessionmaker[AsyncSession] = async_sessionmaker(
@@ -26,8 +26,14 @@ class DataBaseHelper:
         async with self.session_factory() as session:
             yield  session
 
+DEBUG = settings.run.debug
+if not DEBUG:
+    url_db = str(settings.db.url)
+else:
+    url_db = str(settings.db.test_url)
+
 db_helper = DataBaseHelper(
-    url=str(settings.db.url),
+    url=url_db,
     echo=settings.db.echo,
     echo_pool=settings.db.echo_pool,
     max_overflow=settings.db.max_overflow,
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,32 @@
 import pytest
-from contextlib import nullcontext as does_not_raise
 
-from src.main import Calculator
+from api.crud.dish import DishCRUD
+from models.base import Base
+from models.db_helper import db_helper
+from shemas.dish import DishCreate
 
-class TestCalculator:
-    @pytest.mark.parametrize(
-        'x, y, res, expected',[
-            (10, 2, 5, does_not_raise()),
-            (5, 2, 2.5, does_not_raise()),
-            (25, 5, 5, does_not_raise()),
-            (20,0,20, pytest.raises(ZeroDivisionError)),
-             (20, "-4", 0, pytest.raises(TypeError))
+
+@pytest.fixture(scope="session", autouse=True)
+async def prepare_db():
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+@pytest.fixture(scope="function", autouse=True)
+async def session():
+    return await db_helper.session_getter()
+
+@pytest.fixture
+def dishes():
+    return [
+        DishCreate(name="name1", price=23),
+        DishCreate(name="name2", price=23.65),
+        DishCreate(name="name3", price=236),
+        DishCreate(name="name4", price=2343),
         ]
-    )
-    def test_devide(self, x, y, res, expected):
-        with expected:
-            assert Calculator().devide(x, y) == res
 
+class TestDishCRUD:
+
+    async def test_add_dish(self, dishes, session):
+        for dish in dishes:
+            await DishCRUD(session).add_dish(dish)
